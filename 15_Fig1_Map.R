74477f8ef14c2afc1e698d31c870f7b69c64a348
Stephanie Cunningham <stcunnin@syr.edu>
2022-07-25 20:24:06 -0400

    Built map for Supplementary material showing missing locations of e-obs units.

--- 15_Fig1_Map.R
+++ 15_Fig1_Map.R
@@ -40,7 +40,7 @@ grid_df <- grid_df[!(grid_df$group=="23.1" & grid_df$order<350),]
 dat1 <- read.csv("output/interp-gps-data_full.csv")
 dat1 <- dat1[,-1]
 dat1 <- dat1[dat1$animal_id!="LM31F" & dat1$animal_id!="LM17M" & dat1$animal_id!="RP15F",]
-dat1 <- dat1[dat1$julian<=150,]
+# dat1 <- dat1[dat1$julian<=150,]
 
 # xy <- dat1[,c(14,13)]
 xy <- dat1[,c(6,5)]
@@ -99,6 +99,23 @@ ggplot() + geom_path(data = grid_df, aes(x = long, y = lat, group = group), colo
   annotate("label", x=-4000000, y=4800000, label="Projection: Azimuthal Equidistant", fill="white") +
   geom_rect(data=rect, aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax), color="black", fill="white") +
   annotation_custom(grob=inset, xmin=0, xmax=4300000, ymin=-4050000,  ymax=-500000)
+
+
+##### A plot showing where interpolated points are
+
+miss <- dat[dat$missing=="y",]
+
+ggplot() + geom_path(data = grid_df, aes(x = long, y = lat, group = group), color="gray50") +
+  mp + coord_cartesian(xlim=c(0,3750000), ylim=c(500000,2250000)) +
+  geom_line(data=dat, aes(x=x, y=y, group=animal_id), color="#4393c3") +
+  geom_point(data=dat, aes(x=x, y=y, group=animal_id, color=missing, shape=missing), size=3, alpha=0.5) +
+  geom_point(data=miss, aes(x=x, y=y, group=animal_id), size=3, shape=24, fill="#b2182b", color="black") +
+  scale_color_manual(values=c("#4393c3", "#b2182b")) +
+  theme_classic() + theme(axis.title.x=element_blank(), axis.text=element_blank(), axis.ticks=element_blank(),
+                          axis.title.y=element_blank(), panel.border=element_rect(color="black", fill=NA, size=0.5),
+                          legend.position="none") +
+  annotation_scale(location="bl", text_cex=1.4, style="bar")
+
 
 
 
