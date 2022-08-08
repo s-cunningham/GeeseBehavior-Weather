27b858bafe2925c9746f09724a3c860ff8fc9ace
Stephanie Cunningham <stcunnin@syr.edu>
2022-08-08 14:16:32 -0400

    Reclassifying ACC data according to each transmitter type

---
+++ 05_CombinedCTTClassificationCV.R
@@ -0,0 +1,305 @@
+#**********************************************************************************************************************************
+#**********************************************************************************************************************************
+
+# Project: Objective 1
+# Date: 20 December 2018
+# Author: Stephanie Cunningham
+# Description: Classification of CTT dataset - cross validation to determine best method
+
+#**********************************************************************************************************************************
+#**********************************************************************************************************************************
+
+# Load packages
+library(class)
+library(tree)
+library(randomForest)
+library(MASS)
+library(tidyverse)
+library(e1071)
+library(matrixStats)
+
+# Source the function file
+source("R/00_ACCfunctions.R") 
+
+# Read in training data
+ctt.wide <- read.csv("data/new_ctt_continentscombined-wildgrazeonly.csv", stringsAsFactors=FALSE) # CTT data
+ctt.wide <- ctt.wide[,-c(1)]
+ctt.wide$Behavior[ctt.wide$Behavior=="walk"] <- "graze"
+names(ctt.wide)[3:89] <- c(rep("X",29), rep("Y",29), rep("Z",29))
+
+# Randomly select a subset of flight and stationary behaviors (not sure if necessary for CTT)
+set.seed(123)
+ctt.b <- ctt.wide[,1:2]
+x.f <- subset(ctt.wide, Behavior=="fly")
+sam.f <- sample(1:nrow(x.f),size=150, replace=FALSE)
+x.f <- x.f[sam.f,]
+x.s <- subset(ctt.wide, Behavior=="stationary")
+sam.s <- sample(1:nrow(x.s),size=150, replace=FALSE)
+x.s <- x.s[sam.s,]
+ctt.wide <- subset(ctt.wide, Behavior!="fly" & Behavior!="stationary")
+ctt.wide <- rbind(ctt.wide, x.s, x.f)
+ctt.wide %>% group_by(Behavior) %>% dplyr::count()
+
+
+# Calculate summary statistics and scale
+ctt.ss <- accSumStatsCTT(ctt.wide)
+# ctt.ss <- scale(ctt.ss[,2:39])  # need this?
+# ctt.ss <- cbind.data.frame(behavior=ctt.wide$behavior, ctt.ss)
+
+# ctt.ss$behavior[ctt.ss$behavior=="walk"] <- "feed"
+
+
+# How many of each behavior
+ctt.ss %>% group_by(behavior) %>% count()
+
+ctt.ss <- droplevels(ctt.ss)
+
+# Set seed
+set.seed(512)
+# set.seed(100)
+
+# Split dataset into train and test sets (80/20 split)
+samples <- sample(1:nrow(ctt.ss),size=0.8*nrow(ctt.ss))
+ctt.train <- ctt.ss[samples,2:39]
+ctt.test <- ctt.ss[-samples,2:39]
+train.behaviors <- ctt.ss$behavior[samples]
+test.behaviors <- ctt.ss$behavior[-samples]
+
+# Random Forest 
+
+K=10
+folds <- sample(1:K, nrow(ctt.ss), replace=TRUE)
+rf.error <- matrix(0, nrow=1, ncol=K)
+rf.prec <- matrix(0, nrow=4, ncol=K)
+rf.rec <- matrix(0, nrow=4, ncol=K)
+rf.accu <- matrix(0, nrow=4, ncol=K)
+
+for (i in 1:K) {
+  ctt.train <- ctt.ss[folds != i,2:39]
+  behavior <- ctt.ss[folds != i, 1]
+  ot <- cbind(behavior, ctt.train)
+  ctt.test <- ctt.ss[folds == i,2:39]
+  test.behaviors <- ctt.ss[folds==i,1]
+  
+  no.beh <- ncol(ctt.train)
+  ctt.rf <- randomForest(behavior~., data=ot, mtry=sqrt(no.beh), ntree=2000)
+  ctt.pred <- predict(ctt.rf, newdata=ctt.test)
+  rf.error[1,i] <- mean(ctt.pred==test.behaviors) 
+  
+  t3 <- table(ctt.pred, test.behaviors)
+  cv.m3 <- data.frame(modelPerformance(t3)[2])
+  rf.prec[,i] <- as.matrix(cv.m3[2,2:5])
+  rf.rec[,i] <- as.matrix(cv.m3[1,2:5])
+  rf.accu[,i] <- as.matrix(cv.m3[3,2:5])
+}
+
+rms3 <- rowMeans(rf.error)
+rms3
+
+prec.rf <- rowMeans(rf.prec)
+rec.rf <- rowMeans(rf.rec)
+accu.rf <- rowMeans(rf.accu)
+
+##########################################################################################################################
+# K nearest neighbors
+
+KCV=10
+# folds <- sample(1:KCV, nrow(ctt.ss), replace=TRUE)
+knn.error <- matrix(0, nrow=1, ncol=KCV)
+knn.prec <- matrix(0, nrow=4, ncol=KCV)
+knn.rec <- matrix(0, nrow=4, ncol=KCV)
+
+for (i in 1:KCV) {
+  ctt.train <- ctt.ss[folds != i,2:39]
+  train.behaviors <- ctt.ss[folds != i, 1]
+  ctt.test <- ctt.ss[folds == i,2:39]
+  test.behaviors <- ctt.ss[folds==i,1]
+  
+  k=100
+  
+  ctt.k <- matrix(NA, nrow=100, ncol=2)
+  ctt.k[,1] <- seq(1,100,1)
+  
+  for (j in 1:k) {
+    ctt.knn2 <- knn(train=ctt.train, test=ctt.test, cl=train.behaviors, k=j)
+    ctt.k[j,2] <- 1-mean(ctt.knn2==test.behaviors)
+  }
+  
+  # Minimum error rate
+  min(ctt.k[,2])
+  K = min(which(ctt.k[,2]==min(ctt.k[,2])))
+  
+  # Run KNN with best value of k
+  ctt.knn3 <- knn(train=ctt.train, test=ctt.test, cl=train.behaviors, k=K)
+  
+  knn.error[1,i] <- mean(ctt.knn3==test.behaviors) 
+  
+  t1 <- table(ctt.knn3,test.behaviors)
+  cv.m1 <- data.frame(modelPerformance(t1)[2])
+  knn.prec[,i] <- as.matrix(cv.m1[2,2:5])
+  knn.rec[,i] <- as.matrix(cv.m1[1,2:5])
+  
+}
+
+rms1 <- rowMeans(knn.error)
+rms1
+
+prec.knn <- rowMeans(knn.prec)
+rec.knn <- rowMeans(knn.rec)
+
+##########################################################################################################################
+# Classification and regression trees
+
+K=10
+# folds <- sample(1:K, nrow(ctt.ss), replace=TRUE)
+cart.error <- matrix(0, nrow=1, ncol=K)
+cart.prec <- matrix(0, nrow=4, ncol=K)
+cart.rec <- matrix(0, nrow=4, ncol=K)
+
+for (i in 1:K) {
+  ctt.train <- ctt.ss[folds != i,2:39]
+  behavior <- ctt.ss[folds != i, 1]
+  ctt.train <- cbind(behavior, ctt.train)
+  ctt.test <- ctt.ss[folds == i,2:39]
+  test.behaviors <- ctt.ss[folds==i,1]
+  
+  tree.ctt <- tree(behavior~., data=ctt.train)
+  ctt.tree.p <- predict(tree.ctt, newdata=ctt.test, type="class")
+  
+  # Cross validation to determine optimal number of terminal nodes, and then "prune" the tree
+  cv.ctt <- cv.tree(tree.ctt)
+  prune.ctt <- prune.tree(tree.ctt,best=cv.ctt$size[which(cv.ctt$dev==min(cv.ctt$dev))])
+  
+  # Predict on the test set using the pruned tree
+  ctt.prune.p <- predict(prune.ctt, newdata=ctt.test, type="class")
+  
+  cart.error[1,i] <- mean(ctt.prune.p==test.behaviors) 
+  
+  t2 <- table(ctt.prune.p, test.behaviors)
+  cv.m2 <- data.frame(modelPerformance(t2)[2])
+  cart.prec[,i] <- as.matrix(cv.m2[2,2:5])
+  cart.rec[,i] <- as.matrix(cv.m2[1,2:5])
+}
+
+rms2 <- rowMeans(cart.error)
+rms2
+
+prec.cart <- rowMeans(cart.prec)
+rec.cart <- rowMeans(cart.rec)
+
+#************************************************************************************************************************
+# Linear Discriminat Analysis (LDA)
+
+# folds <- sample(1:K, nrow(ctt.ss), replace=TRUE)
+lda.error <- matrix(0, nrow=1, ncol=K)
+lda.prec <- matrix(0, nrow=4, ncol=K)
+lda.rec <- matrix(0, nrow=4, ncol=K)
+
+for (i in 1:K) {
+  ctt.train <- ctt.ss[folds != i,2:39]
+  behavior <- ctt.ss[folds != i, 1]
+  ctt.train <- cbind(behavior, ctt.train)
+  ctt.test <- ctt.ss[folds == i,2:39]
+  test.behaviors <- ctt.ss[folds==i,1]
+  
+  ctt.lda <- lda(behavior~., data=ctt.train)
+  ctt.lda.p <- predict(ctt.lda,  newdata=ctt.test)
+  lda.error[1,i] <- mean(ctt.lda.p$class==test.behaviors)
+  
+  t4 <- table(ctt.lda.p$class, test.behaviors)
+  cv.m4 <- data.frame(modelPerformance(t4)[2])
+  lda.prec[,i] <- as.matrix(cv.m4[2,2:5])
+  lda.rec[,i] <- as.matrix(cv.m4[1,2:5])
+}
+
+rms4 <- rowMeans(lda.error)
+rms4
+
+prec.lda <- rowMeans(lda.prec)
+rec.lda <- rowMeans(lda.rec)
+
+#************************************************************************************************************************
+# support Vector Machines (SVM)
+
+# folds <- sample(1:K, nrow(ctt.ss), replace=TRUE)
+svm.error <- matrix(0, nrow=1, ncol=K)
+svm.prec <- matrix(0, nrow=4, ncol=K)
+svm.rec <- matrix(0, nrow=4, ncol=K)
+
+for (i in 1:K) {
+  ctt.train <- ctt.ss[folds != i,2:39]
+  train.behaviors <- ctt.ss[folds != i, 1]
+  ot <- cbind(train.behaviors, ctt.train)
+  ctt.test <- ctt.ss[folds == i,2:39]
+  test.behaviors <- ctt.ss[folds==i,1]
+  
+  ctt.svm <- svm(train.behaviors~., data=ot, kernel="radial")
+  
+  svm.p <- predict(ctt.svm, newdata=ctt.test)
+  svm.error[1,i] <- mean(svm.p==test.behaviors)
+  
+  t5 <- table(svm.p, test.behaviors)
+  cv.m5 <- data.frame(modelPerformance(t5)[2])
+  svm.prec[,i] <- as.matrix(cv.m5[2,2:5])
+  svm.rec[,i] <- as.matrix(cv.m5[1,2:5])
+}
+
+rms5 <- rowMeans(svm.error)
+rms5
+
+prec.svm <- rowMeans(svm.prec)
+rec.svm <- rowMeans(svm.rec)
+
+
+#************************************************************************************************************************
+# Recreate Precision vs. Recall plot from AcceleRater
+
+rf.pr <- rbind(prec.rf, rec.rf)
+knn.pr <- rbind(prec.knn, rec.knn)
+lda.pr <- rbind(prec.lda, rec.lda)
+cart.pr <- rbind(prec.cart, rec.cart)
+svm.pr <- rbind(prec.svm, rec.svm)
+
+all <- rbind(knn.pr, cart.pr, rf.pr, lda.pr, svm.pr)
+rownames(all) <- seq(1,nrow(all),1)
+all <- as.data.frame(all)
+Method <- c(rep("KNN",2), rep("CART",2), rep("RF",2), rep("LDA",2), rep("SVM",2))
+all <- cbind(Method, all)
+Measure <- rep(c("Precision","Recall"), 5)
+all <- cbind(Measure, all)
+names(all)[3:6] <- c("graze", "walk", "stationary", "fly")
+
+apr <- all %>% gather(key="Behavior", value="Value", 3:6) %>%
+  spread(Measure, Value)
+
+ggplot(apr, aes(x=Recall, y=Precision, shape=Method, colour=Behavior)) + geom_point(size=5) + 
+  coord_cartesian(xlim=c(70,100),ylim=c(70,100)) + theme_bw() + xlab("Recall (%)") + ylab("Precision (%)") +
+  theme(axis.text=element_text(size=14), axis.title=element_text(size=16, face="bold"),
+        legend.text=element_text(size=14), legend.title=element_text(size=16, face="bold")) + 
+  ggtitle("Model Performance - CTT")
+
+
+#************************************************************************************************************************
+# Plot overall accuracy for each method
+
+error <- rbind(knn.error, cart.error, rf.error, lda.error, svm.error)
+Mean <- rowMeans(error)*100
+Median <- rowMedians(error)*100
+SD <- rowSds(error)*100  
+Method <- c("KNN","CART","RF","LDA","SVM")
+error.stats <- data.frame(Method, Mean, Median, SD)
+error.stats
+error <- t(error)
+error <- as.data.frame(error)
+names(error) <- c("KNN","CART","RF","LDA","SVM")
+error$CV <- as.factor(c("CV1","CV2","CV3","CV4","CV5","CV6","CV7","CV8","CV9","CV10"))
+error2 <- gather(error, key="Method", value="CVmean", 1:5)
+error2$CVmean <- error2$CVmean*100
+
+ggplot(error2, aes(x=Method, y=CVmean, fill=Method)) + geom_boxplot() + theme_bw(base_size=16) + scale_fill_discrete(guide=FALSE) + theme(axis.text=element_text(size=14))
+
+
+
+
+
+
---
+++ 05_CombinedORNClassificationCV.R
@@ -0,0 +1,292 @@
+#**********************************************************************************************************************************
+#**********************************************************************************************************************************
+
+# Project: Objective 1
+# Date: 20 December 2018
+# Author: Stephanie Cunningham
+# Description: Combining (NAMC & GRLD) Ornitela training sets (combined continents)
+
+#**********************************************************************************************************************************
+#**********************************************************************************************************************************
+
+# Load packages
+library(class)
+library(tree)
+library(randomForest)
+library(MASS)
+library(e1071)
+library(matrixStats)
+library(tidyverse)
+
+# Source the function file
+source("R/00_ACCfunctions.R")  
+
+# Read in Ornitela data
+orn.wide <- read.csv("data/orn_combined.csv", stringsAsFactors=FALSE) 
+orn.wide <- orn.wide[,-c(1)]
+orn.wide$Behavior[orn.wide$Behavior=="walk"] <- "graze"
+orn.wide %>% group_by(Behavior) %>% count()
+names(orn.wide)[3:92] <- c(rep("X",30), rep("Y",30), rep("Z",30))
+
+# Calculate summary statistics and scale
+orn.ss <- accSumStatsORN(orn.wide)
+
+# How many of each behavior
+orn.ss %>% group_by(behavior) %>% count()
+comb.ss <- orn.ss
+
+comb.ss %>% group_by(behavior) %>% count()
+
+# Set seed
+set.seed(123)
+ 
+# Random Forest 
+K=10
+folds <- sample(1:K, nrow(comb.ss), replace=TRUE)
+rf.error <- matrix(0, nrow=1, ncol=K)
+rf.prec <- matrix(0, nrow=3, ncol=K)
+rf.rec <- matrix(0, nrow=3, ncol=K)
+rf.accu <- matrix(0, nrow=3, ncol=K)
+
+for (i in 1:K) {
+  comb.train <- comb.ss[folds != i,2:39]
+  behavior <- comb.ss[folds != i, 1]
+  ot <- cbind(behavior, comb.train)
+  comb.test <- comb.ss[folds == i,2:39]
+  test.behaviors <- comb.ss[folds==i,1]
+  
+  no.beh <- ncol(comb.train)
+  ctt.rf <- randomForest(factor(behavior)~., data=ot, mtry=sqrt(no.beh), ntree=2000)
+  ctt.pred <- predict(ctt.rf, newdata=comb.test)
+  rf.error[1,i] <- mean(ctt.pred==test.behaviors) 
+  
+  t3 <- table(ctt.pred, test.behaviors)
+  cv.m3 <- data.frame(modelPerformance(t3)[2])
+  rf.prec[,i] <- as.matrix(cv.m3[2,2:4])
+  rf.rec[,i] <- as.matrix(cv.m3[1,2:4])
+  rf.accu[,i] <- as.matrix(cv.m3[3,2:4])
+}
+
+rms3 <- rowMeans(rf.error)
+rms3
+
+prec.rf <- rowMeans(rf.prec)
+rec.rf <- rowMeans(rf.rec)
+accu.rf <- rowMeans(rf.accu)
+
+##########################################################################################################################
+# K nearest neighbors
+
+KCV=10
+# folds <- sample(1:KCV, nrow(comb.ss), replace=TRUE)
+knn.error <- matrix(0, nrow=1, ncol=KCV)
+knn.prec <- matrix(0, nrow=3, ncol=KCV)
+knn.rec <- matrix(0, nrow=3, ncol=KCV)
+
+for (i in 1:KCV) {
+  comb.train <- comb.ss[folds != i,2:39]
+  train.behaviors <- comb.ss[folds != i, 1]
+  comb.test <- comb.ss[folds == i,2:39]
+  test.behaviors <- comb.ss[folds==i,1]
+  
+  k=100
+  
+  ctt.k <- matrix(NA, nrow=100, ncol=2)
+  ctt.k[,1] <- seq(1,100,1)
+  
+  for (j in 1:k) {
+    ctt.knn2 <- knn(train=comb.train, test=comb.test, cl=train.behaviors, k=j)
+    ctt.k[j,2] <- 1-mean(ctt.knn2==test.behaviors)
+  }
+  
+  # Minimum error rate
+  min(ctt.k[,2])
+  K = min(which(ctt.k[,2]==min(ctt.k[,2])))
+  
+  # Run KNN with best value of k
+  ctt.knn3 <- knn(train=comb.train, test=comb.test, cl=train.behaviors, k=K)
+  
+  knn.error[1,i] <- mean(ctt.knn3==test.behaviors) 
+  
+  t1 <- table(ctt.knn3,test.behaviors)
+  cv.m1 <- data.frame(modelPerformance(t1)[2])
+  knn.prec[,i] <- as.matrix(cv.m1[2,2:4])
+  knn.rec[,i] <- as.matrix(cv.m1[1,2:4])
+  
+}
+
+rms1 <- rowMeans(knn.error)
+rms1
+
+prec.knn <- rowMeans(knn.prec)
+rec.knn <- rowMeans(knn.rec)
+
+##########################################################################################################################
+# Classification and regression trees
+
+K=10
+# folds <- sample(1:K, nrow(comb.ss), replace=TRUE)
+cart.error <- matrix(0, nrow=1, ncol=K)
+cart.prec <- matrix(0, nrow=3, ncol=K)
+cart.rec <- matrix(0, nrow=3, ncol=K)
+
+for (i in 1:K) {
+  comb.train <- comb.ss[folds != i,2:39]
+  behavior <- comb.ss[folds != i, 1]
+  comb.train <- cbind(behavior, comb.train)
+  comb.test <- comb.ss[folds == i,2:39]
+  test.behaviors <- comb.ss[folds==i,1]
+  
+  tree.ctt <- tree(factor(behavior)~., data=comb.train)
+  ctt.tree.p <- predict(tree.ctt, newdata=comb.test, type="class")
+  
+  # Cross validation to determine optimal number of terminal nodes, and then "prune" the tree
+  cv.ctt <- cv.tree(tree.ctt)
+  prune.ctt <- prune.tree(tree.ctt,best=cv.ctt$size[which(cv.ctt$dev==min(cv.ctt$dev))])
+  
+  # Predict on the test set using the pruned tree
+  ctt.prune.p <- predict(prune.ctt, newdata=comb.test, type="class")
+  
+  cart.error[1,i] <- mean(ctt.prune.p==test.behaviors) 
+  
+  t2 <- table(ctt.prune.p, test.behaviors)
+  cv.m2 <- data.frame(modelPerformance(t2)[2])
+  cart.prec[,i] <- as.matrix(cv.m2[2,2:4])
+  cart.rec[,i] <- as.matrix(cv.m2[1,2:4])
+}
+
+rms2 <- rowMeans(cart.error)
+rms2
+
+prec.cart <- rowMeans(cart.prec)
+rec.cart <- rowMeans(cart.rec)
+
+#************************************************************************************************************************
+# Linear Discriminat Analysis (LDA)
+
+# folds <- sample(1:K, nrow(comb.ss), replace=TRUE)
+lda.error <- matrix(0, nrow=1, ncol=K)
+lda.prec <- matrix(0, nrow=3, ncol=K)
+lda.rec <- matrix(0, nrow=3, ncol=K)
+
+for (i in 1:K) {
+  comb.train <- comb.ss[folds != i,2:39]
+  behavior <- comb.ss[folds != i, 1]
+  comb.train <- cbind(behavior, comb.train)
+  comb.test <- comb.ss[folds == i,2:39]
+  test.behaviors <- comb.ss[folds==i,1]
+  
+  ctt.lda <- lda(behavior~., data=comb.train)
+  ctt.lda.p <- predict(ctt.lda,  newdata=comb.test)
+  lda.error[1,i] <- mean(ctt.lda.p$class==test.behaviors)
+  
+  t4 <- table(ctt.lda.p$class, test.behaviors)
+  cv.m4 <- data.frame(modelPerformance(t4)[2])
+  lda.prec[,i] <- as.matrix(cv.m4[2,2:4])
+  lda.rec[,i] <- as.matrix(cv.m4[1,2:4])
+}
+
+rms4 <- rowMeans(lda.error)
+rms4
+
+prec.lda <- rowMeans(lda.prec)
+rec.lda <- rowMeans(lda.rec)
+
+#************************************************************************************************************************
+# support Vector Machines (SVM)
+
+# folds <- sample(1:K, nrow(comb.ss), replace=TRUE)
+svm.error <- matrix(0, nrow=1, ncol=K)
+svm.prec <- matrix(0, nrow=3, ncol=K)
+svm.rec <- matrix(0, nrow=3, ncol=K)
+
+for (i in 1:K) {
+  comb.train <- comb.ss[folds != i,2:39]
+  train.behaviors <- comb.ss[folds != i, 1]
+  ot <- cbind(train.behaviors, comb.train)
+  comb.test <- comb.ss[folds == i,2:39]
+  test.behaviors <- comb.ss[folds==i,1]
+  
+  ctt.svm <- svm(factor(train.behaviors)~., data=ot, kernel="radial")
+  
+  svm.p <- predict(ctt.svm, newdata=comb.test)
+  svm.error[1,i] <- mean(svm.p==test.behaviors)
+  
+  t5 <- table(svm.p, test.behaviors)
+  cv.m5 <- data.frame(modelPerformance(t5)[2])
+  svm.prec[,i] <- as.matrix(cv.m5[2,2:4])
+  svm.rec[,i] <- as.matrix(cv.m5[1,2:4])
+}
+
+rms5 <- rowMeans(svm.error)
+rms5
+
+prec.svm <- rowMeans(svm.prec)
+rec.svm <- rowMeans(svm.rec)
+
+
+#************************************************************************************************************************
+# Recreate Precision vs. Recall plot from AcceleRater
+
+rf.pr <- rbind(prec.rf, rec.rf)
+knn.pr <- rbind(prec.knn, rec.knn)
+lda.pr <- rbind(prec.lda, rec.lda)
+cart.pr <- rbind(prec.cart, rec.cart)
+svm.pr <- rbind(prec.svm, rec.svm)
+
+all <- rbind(knn.pr, cart.pr, rf.pr, lda.pr, svm.pr)
+rownames(all) <- seq(1,nrow(all),1)
+all <- as.data.frame(all)
+Method <- c(rep("KNN",2), rep("CART",2), rep("RF",2), rep("LDA",2), rep("SVM",2))
+all <- cbind(Method, all)
+Measure <- rep(c("Precision","Recall"), 5)
+all <- cbind(Measure, all)
+names(all)[3:5] <- c("graze", "stationary", "fly")
+
+apr <- all %>% gather(key="Behavior", value="Value", 3:5) %>%
+  spread(Measure, Value)
+
+ggplot(apr, aes(x=Recall, y=Precision, shape=Method, colour=Behavior)) + geom_point(size=5) + 
+  coord_cartesian(xlim=c(70,100),ylim=c(70,100)) + theme_bw() + xlab("Recall (%)") + ylab("Precision (%)") +
+  theme(axis.text=element_text(size=14), axis.title=element_text(size=16, face="bold"),
+        legend.text=element_text(size=14), legend.title=element_text(size=16, face="bold")) + 
+  ggtitle("Model Performance - Combined CTT & Ornitela")
+
+
+#************************************************************************************************************************
+# Plot overall accuracy for each method
+
+error <- rbind(knn.error, cart.error, rf.error, lda.error, svm.error)
+Mean <- rowMeans(error)*100
+Median <- rowMedians(error)*100
+SD <- rowSds(error)*100  
+Method <- c("KNN","CART","RF","LDA","SVM")
+error.stats <- data.frame(Method, Mean, Median, SD)
+error.stats
+error <- t(error)
+error <- as.data.frame(error)
+names(error) <- c("KNN","CART","RF","LDA","SVM")
+error$CV <- as.factor(c("CV1","CV2","CV3","CV4","CV5","CV6","CV7","CV8","CV9","CV10"))
+error2 <- gather(error, key="Method", value="CVmean", 1:5)
+error2$CVmean <- error2$CVmean*100
+
+ggplot(error2, aes(x=Method, y=CVmean, fill=Method)) + geom_boxplot() + theme_bw(base_size=16) + scale_fill_discrete(guide=FALSE) + theme(axis.text=element_text(size=14))
+
+
+
+######################################################
+
+# Classifying ornitela devices
+
+
+
+
+
+
+
+
+
+
+
+
+
