db50a3f53e8984d9dbf2b3f734ef55b2deb58250
Stephanie Cunningham <stcunnin@esf.edu>
2022-08-25 11:04:14 -0400

    Dynamic linear models for proportion time feeding, changing to binomial model (from ODBA model)

--- 10_DynamicLinearModels_PTF.R
+++ 10_DynamicLinearModels_PTF.R
@@ -57,6 +57,7 @@ nind <- length(unique(dat$animal_id))
 prcp <- matrix(NA, nrow=nind, ncol=dur)
 mintemp <- matrix(NA, nrow=nind, ncol=dur)
 Y <- matrix(NA, nrow=nind, ncol=dur)
+n <- matrix(NA, nrow=nind, ncol=dur)
 beta1 <- matrix(NA, nrow=nind, ncol=dur)
 beta2 <- matrix(NA, nrow=nind, ncol=dur)
 beta3 <- matrix(NA, nrow=nind, ncol=dur)
@@ -66,8 +67,99 @@ for (i in 1:length(un.id)) {
   r <- dim(dat[dat$animal_id==un.id[i],])[1]
   prcp[i,1:r] <- dat[dat$animal_id==un.id[i],19]
   mintemp[i,1:r] <- dat[dat$animal_id==un.id[i],21]
-  Y[i,1:r] <- dat[dat$animal_id==un.id[i],23] # ODBA
+  Y[i,1:r] <- dat[dat$animal_id==un.id[i],13] # Feeding bursts
+  n[i,1:r] <- dat[dat$animal_id==un.id[i],15] # total bursts
   beta1[i,1:r] <- rnorm(r)
   beta2[i,1:r] <- rnorm(r)
   beta3[i,1:r] <- rnorm(r)
 }
+
+# Run each model individually
+sink("R/dlm_ptf_ind.txt")
+cat("
+    model {
+    
+    # Priors
+    beta0 ~ dnorm(0, 0.01)        # Intercept
+
+    mu1 ~ dnorm(0, 0.01)          # beta1[1]
+    mu2 ~ dnorm(0, 0.01)          # beta2[1]
+    
+    eta.p1 ~ dgamma(0.001, 0.001) # Process variance for covariate 1
+    sd.q1 <- 1/sqrt(eta.p1)
+    
+    eta.p2 ~ dgamma(0.001, 0.001) # Process variance for covariate 1
+    sd.q2 <- 1/sqrt(eta.p2)
+
+    # Initialize (fill in first value)
+    beta1[1] <- mu1
+    beta2[1] <- mu2
+
+    predY[1] <- ilogit(beta0 + beta1[1]*prcp[1] + beta2[1]*temp[1])
+    y[1] ~ dbinom(predY[1], n[1])
+
+    # Likelihood
+    for (i in 2:dur) {
+
+      # Process model
+      predX1[i] <- 1*beta1[i-1]
+      beta1[i] ~ dnorm(predX1[i], eta.p1)	# coefficients for precipitation rate
+
+      predX2[i] <- 1*beta2[i-1]
+      beta2[i] ~ dnorm(predX2[i], eta.p2)	# coefficiencts for temperature
+
+      # Observation model
+      predY[i] <- ilogit(beta0 + beta1[i]*prcp[i] + beta2[i]*temp[i])
+      y[i] ~ dbinom(predY[i], n[i])	      # Observation variation
+    }
+    
+    }", fill=TRUE)
+sink()
+
+
+# Run model for each bird
+for (i in 1:length(un.id)) {
+  
+  y <- Y[i,]
+  n <- n[i,]
+  p <- prcp[i,]
+  temp <- mintemp[i,]
+  d <- mdates$duration[i] 
+  
+  b1 <- beta1[i,]
+  b2 <- beta2[i,]
+  bird <- dat$animal_id[i]
+  
+  # Bundle data
+  jags.data <- list(y=y, n=n, prcp=p, temp=temp, dur=d)
+  
+  # Initial values
+  inits <- function() {list(mu1=rnorm(1), mu2=rnorm(1),
+                            eta.p1=1, eta.p2=1, tau.o=1,
+                            beta0=rnorm(1), beta1=b1[1:d], beta2=b2[1:d])}
+  
+  # Parameters monitored
+  params <- c("sd.q1", "sd.q2","beta0", "beta1", "beta2")
+  
+  # MCMC settings
+  ni <- 120000
+  nb <- 80000
+  nt <- 1
+  nc <- 3
+  
+  # Run model
+  out <- jags(jags.data, inits, params, "R/dlm_ptf_ind.txt", n.thin=nt, n.chains=nc, n.burnin=nb, n.iter=ni, parallel=TRUE)
+  
+  # Print R hat
+  print(range(out$Rhat))
+  
+  filename = paste0("results/mtemp-prate_PTF_", bird, ".Rdata" )
+  save(file=filename, list="out")
+  
+  smry <- as.data.frame(out1$summary)
+  
+  csvname = paste0("results/mtemp-prate_dlmPTF_summary_", bird, ".csv" )
+  write_csv(smry, csvname)
+  
+}
+
