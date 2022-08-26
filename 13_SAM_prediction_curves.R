e19fea2df520efff9270f54baf65a27315bd6f3e
Stephanie Cunningham <stcunnin@esf.edu>
2022-08-26 10:15:06 -0400

    Starting to set up posterior samples

--- 13_SAM_prediction_curves.R
+++ 13_SAM_prediction_curves.R
@@ -4,6 +4,22 @@
 ##*   
 ##*
 
-library()
+library(tidyverse)
+# library(patchwork)
+# library(boot)
+
+## ODBA results
+load("results/ODBA_sam.Rdata")
+
+# Set up posterior samples
+beta01 <- c()
+beta02 <- c()
+beta1 <- c()
+beta2 <- c()
+beta3 <- c()
+
+# Predict
+pred_length <- 100
+wint_pred <- seq(,,length.out=pred_length)
 
 
