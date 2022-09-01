f3d635ebdc581e69acf5cb391160b1c0038b64fe
Stephanie Cunningham <stcunnin@esf.edu>
2022-09-01 13:59:54 -0400

    Trying to subtract from monthly value

--- 07_extracting_precipitation.R
+++ 07_extracting_precipitation.R
@@ -1,6 +1,7 @@
 library(ncdf4)
 library(raster)
 library(tidyverse)
+library(forcast)
 
 # List files
 files <- list.files(path="data/gridded_precip/", pattern=".nc", all.files=TRUE, full.names=TRUE)
@@ -51,7 +52,63 @@ range(prcp$prcp)
 
 prcp$prcp[prcp$prcp==-99999] <- NA
 
+### For day with missing data, subtract from the monthly value
+
+missing_id <- prcp[is.na(prcp$prcp),"animal_id"]
+
+# Subset file list to only April 2013
+files <- files[grep("201304", files)]
+
+for (i in 1:length(missing_id)) {
+  
+  month <- data.frame(date=seq.Date(as.Date("2013-04-01"), as.Date("2013-04-30"), by="day"),
+                      prcp=rep(NA,30))
+  
+  # subset goose data
+  temp <- dat[dat$animal_id==missing_id[i],]
+  temp <- temp[temp$date==as.Date("2013-04-06"),] %>% as.data.frame()
+  
+  # convert to spdf
+  xy <- temp[,c(6,5)]
+  spdf <- SpatialPointsDataFrame(coords=xy, data=temp, proj4string=CRS("+proj=longlat +datum=WGS84"))
+  
+  # Loop over days
+  for (j in 1:length(files)) {
+    
+    b <- brick(files[j])
+    
+    # Extract from raster
+    w <- raster::extract(b, spdf, method='bilinear', fun='mean', df=TRUE)
+    
+    month[j,2] <- w[1,2]
+    
+  }
+  
+  month$prcp[month$prcp<0] <- NA
+  
+  # Read in monthly file
+  m <- brick("data/gridded_precip/monthly/gpcp_v02r03_monthly_d201304_c20170616.nc")
+  
+  # Extract from raster
+  w <- raster::extract(m, spdf, method='bilinear', fun='mean', df=TRUE)
+  
+  
+  
+}
+
+
+
+
+plot(b)
+points(temp$longitude, temp$latitude, pch=16)
+unique(b)
+
+
+
+
+
+
+
+
 # Write data
 write_csv(prcp, "files_for_models/daily_precip.csv")
-
-
